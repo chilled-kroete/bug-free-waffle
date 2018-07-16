c17278dc62a90a6e020f80e1a2c6dd90b35a80a4
Maximilian Ratz <maximilian.ratz@gmail.com>
2018-07-16 20:26:33 +0200

    streamlined positioning // switched to HSV color space

--- src/main.cpp
+++ src/main.cpp
@@ -14,46 +14,19 @@
 #define FRAMES 10
 #define FRAME_TIME 1000 / FRAMES
 
-#define STEPSIZE 64
+#define STEPSIZE 32
 #define HALF_STEPSIZE STEPSIZE / 2
 
 #define CONTROLLER WS2812B
 
 CRGB leds[NUM_LEDS];
 
-uint8_t gimmeColor(uint8_t value) {
-    return (uint8_t)((float)value / WIDTH * 255) + 1;
-}
 
-uint8_t gimmeNextColor(int16_t prev) {
-    prev += random(1, STEPSIZE) - HALF_STEPSIZE;
 
-    return max(10, min(255, prev));
-}
-
-uint8_t gimmeNextColor(int16_t prev, int16_t left) {
-    int16_t avg = (prev + left) / 2;
-
-    return gimmeNextColor(avg);
-}
-
-uint8_t gimmeNextColor(int16_t prev, int16_t left, int16_t up) {
-    int16_t avg = (left + up + prev) / 3;
-
-    return gimmeNextColor(avg);
-}
-
-void draw() {
-    for(int row = 0; row < HEIGHT; row++) {
-        for (int column = 0; column < WIDTH; column++) {
-            if (row % 2 == 0) {
-                leds[row * WIDTH + column] = CRGB::Red;
-            } else {
-                leds[(row + 1) * WIDTH - column - 1] = CRGB::Green;
-            }
-        }
-    }
-}
+/********************************************
+ *  positioning 
+ * 
+ */
 
 bool isFirstRow(int row) {
     return row == 0;
@@ -62,6 +35,43 @@ bool isFirstRow(int row) {
 bool isFirstColumn(int column) {
     return column == 0;
 }
+
+
+
+/********************************************
+ *  coloring
+ * 
+ */
+
+CRGB getAverage(CRGB prev, CRGB leftOrUp) {
+    return CRGB(
+        (prev.r + leftOrUp.r) / 2,
+        (prev.g + leftOrUp.g) / 2,
+        (prev.b + leftOrUp.b) / 2
+    );
+}
+
+CRGB getAverage(CRGB prev, CRGB left, CRGB up) {
+    return CRGB(
+        (left.r + up.r + prev.r) / 3,
+        (left.g + up.g + prev.g) / 3,
+        (left.b + up.b + prev.b) / 3
+    );
+}
+
+CRGB modifyColor(CRGB baseValue) {
+    CHSV hsv = rgb2hsv_approximate(baseValue);
+    hsv.hue += random(1, STEPSIZE) - HALF_STEPSIZE;
+    return CRGB(hsv);
+}
+
+
+/********************************************
+ *  Main iterative stuff 
+ * 
+ */
+
+
 
 void determineNextColor(int row, int column) {
     int current;
@@ -78,34 +88,29 @@ void determineNextColor(int row, int column) {
         previous = current + 1;
         up = (row - 1) * WIDTH + column;
     }
-    
+
+    CRGB base;
     if (isFirstRow(row)) {
         if (isFirstColumn(column)) {
-            leds[current].setRGB(
-                gimmeNextColor(leds[current].r),
-                gimmeNextColor(leds[current].g),
-                gimmeNextColor(leds[current].b)
-            );
+            base = leds[current];
         } else {
-            leds[current].setRGB(
-                gimmeNextColor(leds[current].r, leds[previous].r),
-                gimmeNextColor(leds[current].g, leds[previous].g),
-                gimmeNextColor(leds[current].b, leds[previous].b)
-            );
+            base = getAverage(leds[current], leds[previous]);
         }
     } else {
         if (isFirstColumn(column)) {
-            leds[current].setRGB(
-                gimmeNextColor(leds[current].r, leds[up].r),
-                gimmeNextColor(leds[current].g, leds[up].g),
-                gimmeNextColor(leds[current].b, leds[up].b)
-            );
+            base = getAverage(leds[current], leds[up]);
         } else {
-            leds[current].setRGB(
-                gimmeNextColor(leds[current].r, leds[up].r, leds[previous].r),
-                gimmeNextColor(leds[current].g, leds[up].g, leds[previous].g),
-                gimmeNextColor(leds[current].b, leds[up].b, leds[previous].b)
-            );
+            base = getAverage(leds[current], leds[up], leds[previous]);
+        }
+    }
+
+    leds[current] = modifyColor(base);
+}
+
+void draw() {
+    for(int row = 0; row < HEIGHT; row++) {
+        for (int column = 0; column < WIDTH; column++) {
+            leds[row * WIDTH + column] = CRGB::Purple;
         }
     }
 }
@@ -118,9 +123,16 @@ void iterate() {
     }
 }
 
+/********************************************
+ *  Arduino Callbacks 
+ * 
+ */
+
 void setup() {
+    FastLED.setBrightness(255 / 4);
     FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
 
+draw();
     FastLED.show();
 }
 
@@ -133,4 +145,3 @@ void loop() {
 
     delay(FRAME_TIME - time);
 }
-
